8a285e8f6e9582d46ee9c355bc9096a97424ca74
Badre El Mourabit <badreprocontact@gmail.com>
2024-03-11 13:07:20 +0100

    #

--- v0/main.py
+++ v0/main.py
@@ -1,4 +1,4 @@
-"""Ce fichier est le point d'entrée de l'application. Il permet de lancer l'application NoteIt.
+"""Ce fichier est le point d'entrée de l'application. Il permet de lancer l'application Note-it.
 On crée une instance de la classe DatabaseHandler, qui permet de gérer la base de données.
 On crée ensuite les tables de la base de données, si elles n'existent pas déjà.
 On appelle ensuite la fonction start du module homeController, qui permet de lancer l'application.
